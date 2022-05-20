48a2cef6fbbf8fc074b45130d54d6c7dd29d5648
Krisostomos Abdi <c2007f0682@bangkit.academy>
2022-05-20 12:28:50 +0700

    Adding ayam bakar and bandeng presto recipe

--- Food-data-API/main/food.js
+++ Food-data-API/main/food.js
@@ -1,11 +1,11 @@
 /* eslint-disable max-len */
 const foods = [{
-  id: 'friedrice',
-  name: 'Fried Rice',
-  description: 'Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment ',
-  ingredients: 'Cooked and chilled rice, Eggs, Carrots, onions, green onions and peas, Garlic, Soy sauce, Oyster sauce, Toasted sesame oil, Butter',
+  id: 'ayambakar',
+  name: 'Ayam Bakar',
+  description: 'Ayam bakar is an Indonesian and Malaysian dish, consisting of charcoal-grilled chicken. Ayam bakar literally means "roasted chicken" in Indonesian and Malay. In Java, the chicken is usually marinated with the mixture of kecap manis (sweet soy sauce) and coconut oil, applied with a brush during grilling. The bumbu spice mixture may vary among regions, but usually it consists of combination of ground shallot, garlic, chilli pepper, coriander, tamarind juice, candlenut, turmeric, galangal and salt. In Java, ayam bakar usually tastes rather sweet because of the generous amount of sweet soy sauce either as marination or dipping sauce, while the ayam bakar Padang, Bali, Lombok and most of Sumatra are usually spicier and more reddish in colour due to the generous amount of chilli pepper, turmeric and other spices, and the absence of sweet soy sauce. The chicken pieces are usually partially cooked in the spice mixture using a small fire prior to grilling, in order for the chicken to absorb the spices. During grilling process the remaining spices are applied upon the chicken. Ayam bakar is usually served with sambal terasi (chilli with terasi) or sambal kecap (sliced chilli and shallot in sweet soy sauce) as dipping sauce or condiment and slices of cucumber and tomato as garnishing.',
+  ingredients: '800 g (1 head) chicken, cut into 4 parts, 4 bay leaves, 2 lemongrass stalks, bruised, 20 g tamarind, dissolved in 3 tablespoons hot water, 500 ml coconut milk, 2 tbsp oil, for frying, 10 red onions, 5 cloves of garlic, 1 tbsp coriander (roasted), 5 pecans, half tsp pepper powder, 3 cm turmeric, 50 g brown sugar, 1 tsp salt, 1 tsp sugar, 1 tbsp Bango Sweet Soy Sauce',
   // Paragraph method
-  howtocook: 'First, put about 6 cups of rice into your rice cooker. Let it steam until it is already cooked.Second, wash the vegetables, then dice the carrots and onions into small pieces. Set them aside for the next step. Third, add oil and heat up to 100 degrees. Fourth, toss the vegetables into the pan for about 3 minutes. Then toss in the carrots and onions for 3 minutes with the vegetables. After that add 1 teaspoon of salt into the pan. Fifth, boil the chicken or shrimp with the rest of the ingredients (optional). Sixth, put a bit more oil into the frying pan. And then toss the rice in carefully. Seventh, add an egg and mix it with the other ingredients. After that add approximately 2 to 3 tablespoons of soy sauce while frying. And for the last, put fried rice on a dish and it is ready to serve!',
+  howtocook: "First, Heat oil, saute ground spices, bay leaf, and lemongrass until fragrant. Second, Add the tamarind water and chicken, mix well. Third, Pour in the coconut milk, stir gently so that the coconut milk doesn't break.Fourth, Cook until the coconut milk is absorbed and the chicken is tender. Remove and set aside. Fifth, Heat the pan, grill the chicken while smeared with ground spices and soy sauce. Bake until brown. Last, Lift and serve.",
   // Step by step method
   // first: 'First, put about 6 cups of rice into your rice cooker. Let it steam until it is already cooked.',
   // second: 'Second, wash the vegetables, then dice the carrots and onions into small pieces. Set them aside for the next step.',
@@ -15,6 +15,34 @@ const foods = [{
   // sixth: 'Sixth, put a bit more oil into the frying pan. And then toss the rice in carefully.',
   // seventh: 'Seventh, add an egg and mix it with the other ingredients. After that add approximately 2 to 3 tablespoons of soy sauce while frying.',
   // eighth: 'And for the last, put fried rice on a dish and it is ready to serve!',
+},
+{
+  id: 'bandengpresto',
+  name: 'Bandeng Presto ',
+  description: 'Bandeng presto is a typical Indonesian food originating from Juwana District, Pati Regency, Central Java and Gresik, East Java. This dish is made from milkfish (Chanos chanos) seasoned with garlic, turmeric and salt.This milkfish is then cooked on a banana leaf base in a pressure cooker. Presto is a method of cooking with high pressure steam. Food cooked this way is placed in a pan that can be locked tightly. The water in this pot is then heated until it boils. The water vapor that arises will cook the food in this pan. Because milkfish are known to have many spines, pressure cookers are a favorite food because with this method of cooking, these spines become very soft.',
+  ingredients: '5 whitefish (weighing 200 grams each), clean and wash, 5 bay leaves, 1 tbsp red onion, puree, 1 tsp garlic, puree, 1 tbsp salt, grind, 1 tsp turmeric sliced, puree, 300 ml water',
+  howtocook: 'First, Take a basin, put the milkfish in it. Coat with ground spices except bay leaf. Leave it for an hour. Second, Prepare a pressure cooker, line the bottom with bay leaves. Arrange the seasoned milkfish, pour water. Close presto tightly, lock. Third, Cook the milkfish for 60 minutes. Lift. Fourth, Open the temperature control valve, let the steam out until it runs out. Pick up the fish. Serve the milkfish presto while warm with rice or fried first.',
+},
+{
+  id: '',
+  name: '',
+  description: '',
+  ingredients: '',
+  howtocook: '',
+},
+{
+  id: '',
+  name: '',
+  description: '',
+  ingredients: '',
+  howtocook: '',
+},
+{
+  id: '',
+  name: '',
+  description: '',
+  ingredients: '',
+  howtocook: '',
 },
 {
   id: '',
